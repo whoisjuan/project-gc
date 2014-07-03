39c7749f663ad154d6115cadf5e26fbcec54142d
Juan Jose Ramirez U <jjramirez.u@gmail.com>
2014-07-03 16:20:33 -0400

    GoalCulture FrontEnd v.1

---
+++ GC/js/custom.js
@@ -0,0 +1,25 @@
+/*Toggle Side Bar*/
+
+/*$("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("active");
+});*/
+
+$(function () {
+    $('a[href="#search"]').on('click', function(event) {
+        event.preventDefault();
+        $('#search').addClass('open');
+        $('#search > form > input[type="search"]').focus();
+    });
+    
+    $('#search, #search button.close').on('click keyup', function(event) {
+        if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
+            $(this).removeClass('open');
+        }
+    });
+    
+    $('form').submit(function(event) {
+        event.preventDefault();
+        return false;
+    })
+});
